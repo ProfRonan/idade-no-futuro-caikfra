6cd98ab40efe237ec5f6d33f1db0000530aa0f8c
Caik Franco <caikfra@alu.ufc.br>
2023-03-27 21:18:21 -0300

    Arquivo main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+anoatual = int(input())
+idadeusuário = int(input())
+outroano = int(input())
+nome = (input())
+idade = (anoatual - outroano) + idadeusuário
+print(nome,","'no ano de', anoatual, "você terá", idade, "anos")
