f2949e864b86d7d9f2187c377ef5762b3518771f
Caik Franco <128155208+caikfra@users.noreply.github.com>
2023-03-27 22:22:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 anoatual = int(input())
 idadeusuário = int(input())
 outroano = int(input())
-nome = (input())
+nome = input()
+virgula = ','
 idade = (anoatual - outroano) + idadeusuário
-print(nome, anoatual,  idade)
+print(nome + virgula, 'no ano de', anoatual, 'você terá', idade, 'anos')
